c488420b8450045a313f5ac3c6dcaa435d5d7edf
hanjiwon1108 <jwjwhan1108@gmail.com>
2025-09-17 16:00:42 +0900

    chore: 환경설정

---
+++ src/main/java/com/example/transitlove/TransitLoveApplication.java
@@ -0,0 +1,13 @@
+package com.example.transitlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitLoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransitLoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/transitlove/TransitLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transitlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitLoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
